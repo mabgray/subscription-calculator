a999ee9e20257958bf78239989744e584cd92f10
mab_gray <m2bbie@gmail.com>
2023-03-13 13:05:20 -0500

    Event listeners

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 // Your JS code here
 var subTypeElement = document.querySelector("#subscription");
-var subdurationElement = document.querySelector("#months");
+var subDurationElement = document.querySelector("#months");
 var result = document.querySelector(".result");
 var subType = "basic"
 var subDuration = 1;
+
+subTypeElement.addEventListener("change", function(e){
+    //the <option> is the target and we are grabbing its value
+    subType = e.target.value;
+
+    console.log(subType);
+});
+
+subDurationElement.addEventListener("change", function(e){
+    subDuration = Number(e.target.value);
+    console.log(subDuration);
+});
+
