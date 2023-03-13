08ec8ddc5f2c9059d8e6068edd0d57668323a410
mab_gray <m2bbie@gmail.com>
2023-03-13 13:18:14 -0500

    Call back function

--- js/script.js
+++ js/script.js
@@ -9,11 +9,21 @@ subTypeElement.addEventListener("change", function(e){
     //the <option> is the target and we are grabbing its value
     subType = e.target.value;
 
-    console.log(subType);
+   
 });
 
 subDurationElement.addEventListener("change", function(e){
     subDuration = Number(e.target.value);
-    console.log(subDuration);
+  
 });
 
+var updateSubscriptionDiv = function(){
+    monthlyCost = 5;
+     if(subType === "standard"){
+        monthlyCost = 7;
+     } else if(subType === "premium"){
+        monthlyCost = 10;
+     }
+     var total = subDuration * monthlyCost;
+     result.innerText = `you have selected ${sub} with Duration with a montly cost of $${subType}. Your total will be $${total} `
+}
